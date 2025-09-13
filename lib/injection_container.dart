bab60ae36da38ec06c3a91403cb2cf51272956a7
Sawssen Gharbi <sawssen.gharbi@esprit.tn>
2025-09-13 15:24:50 +0100

    Added DI and Multi bloc provider.

---
+++ lib/injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+import 'package:mini_court_book/features/facilities/data/repositories_impl/facility_repository_impl.dart';
+import 'package:mini_court_book/features/facilities/domain/repositories/facility_repository.dart';
+import 'package:mini_court_book/features/facilities/presentation/blocs/bloc/facility_bloc.dart';
+
+final serviceLocator = GetIt.instance;
+Future<void> initDependencies() async {
+  //Blocs
+  serviceLocator.registerFactory<FacilityBloc>(
+    () => FacilityBloc(serviceLocator<FacilityRepository>()),
+  );
+
+  //Repositories
+  serviceLocator.registerLazySingleton<FacilityRepository>(
+    () => FacilityRepositoryImpl(facilityLocalDataSource: serviceLocator()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_court_book/core/theme/theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mini_court_book/features/facilities/presentation/blocs/bloc/facility_bloc.dart';
 import 'package:mini_court_book/features/facilities/presentation/pages/home_screen.dart';
+import 'package:mini_court_book/injection_container.dart';
 
-void main() =>
-    runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
+  runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,13 +24,18 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          title: 'Mini CourtBook',
-          theme: AppTheme.theme,
-          home: HomeScreen(),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => serviceLocator<FacilityBloc>()),
+          ],
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            title: 'Mini CourtBook',
+            theme: AppTheme.theme,
+            home: HomeScreen(),
+          ),
         );
       },
     );
