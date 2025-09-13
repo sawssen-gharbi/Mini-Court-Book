9af901bab5720cbc1066416bc3b136ee5b146721
Sawssen Gharbi <sawssen.gharbi@esprit.tn>
2025-09-13 11:03:03 +0100

    Added device preview setup in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_court_book/core/theme/theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() =>
+    runApp(DevicePreview(enabled: true, builder: (context) => MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Mini CourtBook',
       theme: AppTheme.theme,
       home: SizedBox(),
